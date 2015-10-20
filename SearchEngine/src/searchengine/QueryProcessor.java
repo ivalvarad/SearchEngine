c9fdbc3c37152737df12af06a0218518a9e0e2c1
oscarCastroE <oscar19d@hotmail.es>
2015-10-20 11:27:24 -0600

    no message

--- SearchEngine/src/searchengine/QueryProcessor.java
+++ SearchEngine/src/searchengine/QueryProcessor.java
@@ -24,98 +24,103 @@ public class QueryProcessor {
     //then it has to return the results of the query to the controller
     
     private Index index;
-	private ArrayList<String> stopwords;
+    private ArrayList<String> stopwords;
     
-    public QueryProcessor(Index index){
+    public QueryProcessor(Index index)
+    {
         this.index = index; 
     }
     
     //receives String with the query
     //returns an ArrayList with the id of the documents found
     public ArrayList<String> processQuery(String query)
-	{
-        ArrayList<String> result = new ArrayList<String>();
-		ArrayList<String> queryWords = new ArrayList<String>();
-		// ignore white-spaces and all that stuff.
-		queryWords = separateWords(query);
-		// eliminates the stop-words.
-		queryWords = processWords(queryWords);
+    {
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> queryWords = new ArrayList<>();
+	// ignore white-spaces and all that stuff.
+	queryWords = separateWords(query);
+	// eliminates the stop-words.
+	queryWords = processWords(queryWords);
         result.add("Hola");
         return result;
     }
 	
-	// generates an array of words according to a query that was originally a simple string of chars.
-	public ArrayList<String> separateWords(String query)
+    // generates an array of words according to a query that was originally a simple string of chars.
+    public ArrayList<String> separateWords(String query)
+    {
+        ArrayList<String> result = new ArrayList<>();	
+	char c;
+	String aux;
+	int pos1 = 0;
+	int pos2;
+	for(int i = 0; i < query.length(); ++i)
 	{
-		ArrayList<String> result = new ArrayList<String>();	
-		char c = '';
-		string aux = "";
-		int pos1 = 0;
-		int pos2;
-		for(int i = 0; i < query.lenght(); ++i)
+            c = query.charAt(i);
+            // white spaces are ignored.
+            if(Character.isWhitespace(c) == true)
+            {
+                // generates a substring.
+		pos2 = i-1;
+		aux = query.substring(pos1, pos2);
+		// saves the sub-string.
+		result.add(aux);
+		// ignores the white-spaces between words.
+		c = query.charAt(i+1);
+		while(Character.isWhitespace(c) == true)
 		{
-			c = query.charAt(i);
-			// white spaces are ignored.
-			if(isWhitespace(c) == true)
-			{
-				// generates a substring.
-				pos2 = i-1;
-				aux = query.substring(pos1, pos2);
-				// saves the sub-string.
-				result.add(aux);
-				// ignores the white-spaces between words.
-				c = query.charAt(i+1);
-				while(isWhitespace(c) == true)
-				{
-					++i;
-				}
-				pos1 = i+1;
-			}			
+                    ++i;
 		}
-		return result;		
+                pos1 = i+1;
+            }			
 	}
+	return result;		
+    }
 	
-	// checks if the word has to be ignored or not.
-	// none stop-word will be processed.
-	// could generate an empty array of words for being processed as a query.
-	public ArrayList<String> processWords(ArrayList<String> queryWords)
+    // checks if the word has to be ignored or not.
+    // none stop-word will be processed.
+    // could generate an empty array of words for being processed as a query.
+    public ArrayList<String> processWords(ArrayList<String> queryWords)
+    {
+        ArrayList<String> result = new ArrayList<String>();
+	for(int i = 0; i < queryWords.size(); ++i)
 	{
-		ArrayList<String> result = new ArrayList<String>();
-		for(int i = 0; i < queryWords.getLength(); ++i)
+            boolean includeW = true;
+            for(int j = 0; i < stopwords.size() && includeW != false; ++j)
+            {
+                if((queryWords[i]).compareToIgnoreCase(stopwords[j]) == true))
 		{
-			boolean includeW = true;
-			for(int j = 0; i < stopwords.getLength() && includeW != false; ++j)
-			{
-				if(queryWords[i].compareToIgnoreCase(stopwords[j]) == true)
-				{
-					// this word in the query will be not taken into account.
-					includeW = false;					
-				}
-			}
-			if(includeW == true)
-			{
-				result.add(queryWords[i]);				
-			}
+                    // this word in the query will be not taken into account.
+                    includeW = false;					
 		}
-		return result;		
+            }
+            if(includeW == true)
+            {
+                result.add(queryWords[i]);				
+            }
 	}
+	return result;		
+    }
 
-	// processes a file with the stop-words
-	// and loads them to memory.
-	// stop-words are supposed to be one in each line of the file.
-	public void processStopWords(String path) throws FileNotFoundException
-	{
-		BufferedReader br = new BufferedReader(new FileReader(path));
-        try {
+    // processes a file with the stop-words
+    // and loads them to memory.
+    // stop-words are supposed to be one in each line of the file.
+    public void processStopWords(String path) throws FileNotFoundException
+    {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try
+        {
             String line = br.readLine();
-            while (line != null) {
+            while (line != null) 
+            {
                 // obtains the whole line and saves it in the list of stop-words.
-				line = br.readLine();
-				stopwords.add(line);				
+		line = br.readLine();
+		stopwords.add(line);				
             }
             br.close();
-        } catch (IOException ex) {
+        } 
+        catch (IOException ex)
+        {
             Logger.getLogger(QueryProcessor.class.getName()).log(Level.SEVERE, null, ex);
         }
-	}
+    }
 }
